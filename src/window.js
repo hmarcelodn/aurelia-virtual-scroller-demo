2453e0baa795e26b4eb5771490f5c8e6fdb02c5b
Hugo Marcelo Del Negro <hdelnegro@belatrix.com>
2017-04-10 16:01:43 -0300

    1.0.0-beta-1.0.8

--- src/window.js
+++ src/window.js
@@ -33,9 +33,13 @@ export class Window{
     buildRowCallback(item){
         return "<div class='row'>" +
                  "<div class='col-sm-4'><img class='img-responsive' src='http://vignette3.wikia.nocookie.net/wreckitralph/images/5/58/Blinky8bit.png/revision/latest?cb=20130625170114' class='img-rounded' alt='Red Phantom' width='40' height='40'></div>" +
-                 "<div class='col-sm-4 text-center'>${propertyOne}</div>" +
+                 "<div class='col-sm-4 text-center'><a href='#' click.trigger='navigate()'>${propertyOne}</a></div>" +
                  "<div class='col-sm-4'><input class='form-control' type='text' value.bind='propertyOne' /></div>" +
                "</div>";     
+    }
+
+    navigate(){
+        alert("Thanks!");
     }
 
 }
